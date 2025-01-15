2d81b645e04aa13375b55526809ad326c2e77d38
DSGS76 <duvansalinas1907@gmail.com>
2025-01-14 20:25:55 -0500

    Initial commit with the basics to start with the AeroDream project.

---
+++ src/main/java/com/pa/aerodream/AeroDreamApplication.java
@@ -0,0 +1,13 @@
+package com.pa.aerodream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroDreamApplication.class, args);
+	}
+
+}
